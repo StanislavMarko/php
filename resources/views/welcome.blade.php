a51fd514b5b5f1cd3e0c1ceadd8a1f5f0cd140c5
StanislavMarko <stas.m@intobi.com>
2019-12-02 13:18:05 +0200

    init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    <h1>Here we go!!</h1>
+
+    <p>
+        <a href="/contact">Contact</a> us to learn more.
+    </p>
+</body>
+</html>
