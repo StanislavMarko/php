e8eb0104616d0758202723dbac1fabeabc48464f
StanislavMarko <stas.m@intobi.com>
2019-12-05 14:31:29 +0200

    new function

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,6 +5,8 @@
 @endsection
 @section('content')
     <h1 class="title">Our contacts</h1>
+    <p>Lorem ipsum, cursus eget nam rutrum morbi odio, ligula duis et nam magna gravida diam sodales eu, vulputate rutrum diam risus ligula. Sodales porttitor commodo maecenas nulla eget malesuada nibh nam porta curabitur tellus nec mauris nam leo molestie auctor, morbi fusce malesuada diam tellus magna. Massa rutrum integer magna porttitor in nulla porttitor non risus enim morbi eu: duis diam arcu ornare sapien, sit mattis arcu auctor sit.
 
+        Nibh mauris, nibh ultricies quam, sed — mauris leo ligula, mattis eros, donec, pharetra proin sapien quisque congue tempus urna molestie tempus porttitor donec. Curabitur lorem maecenas a, risus odio donec a gravida arcu nec eget morbi: lorem maecenas. Amet pellentesque ornare diam porttitor, porta odio, fusce nec risus diam ligula massa congue nulla sodales leo: tempus amet risus ipsum, commodo justo. Amet integer donec sem pellentesque: leo metus, non quisque proin. Tellus sodales lorem, leo massa, ultricies quisque leo fusce: adipiscing  mattis tempus arcu — eu maecenas vitae — porta nam congue. Quam pellentesque porta — mauris lorem gravida mattis sodales pellentesque vitae quam eros malesuada quisque vitae, lectus magna orci nibh sagittis lorem ipsum. Magna pharetra arcu ligula — mattis ornare integer, orci risus enim nulla bibendum at non massa vitae at tellus.</p>
 @endsection
 
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -7,13 +7,53 @@
 @section('content')
     <h1 class="title">Projects (<a href="/projects/create">create new</a>)</h1>
 
-    <ul>
-        @foreach ($projects as $project)
-            <li>
-                <a href="/projects/{{ $project->id }}">
-                {{ $project->title }} ({{ $project->description }})
-                </a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="bd-example bd-example-tabs">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            @foreach ($projects as $i => $project)
+                <li class="nav-item">
+                    <a class="nav-link @if ($i == 0) active @endif" id="tab-{{ $project->id }}" data-toggle="tab"
+                       href="#href_{{ $project->id }}" role="tab" aria-controls="aria_{{ $project->id }}" aria-selected="@if ($i == 0) true @else false @endif">
+                        {{ $project->title }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            @foreach ($projects as $i => $project)
+                <div class="tab-pane fade @if ($i == 0) active show @endif" id="href_{{ $project->id }}" role="tabpanel" aria-labelledby="aria_{{ $project->id }}-tab">
+                    <p>
+                        {{ $project->description }}
+                    </p>
+                    <a class="btn btn-primary" href="/projects/{{ $project->id }}/edit" role="button">Edit</a>
+                    <form method="POST" action="/projects/{{ $project->id }}">
+                        @method('DELETE')
+                        @csrf
+                        <div class="field">
+                            <div class="control"><button type="button" class="btn btn-primary btn-danger" data-toggle="modal" data-target="#exampleModal">Delete Project</button></div>
+                        </div>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Hey</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Do you want delete this project ?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                                        <button type="submit" class="btn btn-primary">Yes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
